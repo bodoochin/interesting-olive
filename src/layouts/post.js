da0c06631a1b3dd68329af5838cc37c56bf11eb4
Stackbit Code Editor <projects@stackbit.com>
2021-04-15 17:50:24 +0000

    post.js: changed

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -26,7 +26,7 @@ export default class Post extends React.Component {
                   </div>
                   )}
                   <div className="post-content">
-                      <script src="//my.visme.co/visme-embed.js"></script><div class="visme_d" data-url="q6xj640r-untitled-project" data-w="2560" data-h="1440" data-domain="my"></div><p style="width:142px !important;border-radius:3px !important;padding:3px !important;font-size:12px !important;font-family:Arial, sans-serif !important;color:#314152 !important;white-space:nowrap !important">Made with <a href="https://www.visme.co/?vc=Made-With-Visme&amp;utm_medium=Embed" target="_blank" rel="noreferrer" style="font-weight:600 !important;text-decoration:none !important;font-size:12px !important;font-family:Arial, sans-serif !important;color:#314152 !important;white-space:nowrap !important">Visme</a></p>
+                      
                     {markdownify(_.get(this.props, 'page.markdown', null))}
                   </div>
                   <BlogPostFooter {...this.props} page={this.props.page} date_type={'long'} />
