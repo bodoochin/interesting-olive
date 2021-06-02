ee5dd94741c61269c4920ac450b91e69ab2f8f29
bodoochin <56536664+bodoochin@users.noreply.github.com>
2021-06-02 17:57:12 +0800

    Update _document.js

--- _document.js
+++ _document.js
@@ -3,9 +3,11 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
+    {
     return (
       <Html>
-        <Head />
+        <Head /> 
+      <script data-ad-client="ca-pub-2046685359059262" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <body>
           <Main />
           <NextScript />
