7bdfc6f2d3675b72a2f57beebee6934aaed7822a
bodoochin <56536664+bodoochin@users.noreply.github.com>
2021-06-02 17:56:04 +0800

    Update _document.js

--- _document.js
+++ _document.js
@@ -3,37 +3,9 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
-  render() {
     return (
       <Html>
-        <Head /> <script data-ad-client="ca-pub-2046685359059262" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                  import React, { useEffect } from "react";
-
-const AdBanner = () => {
-  useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
-
-  return (
-    <ins
-      className="adsbygoogle adbanner-customize"
-      style={{
-        display: "block"
-      }}
-      data-ad-client=<your-client-id>
-      data-ad-slot=<slot-id>
-    />
-  );
-};
-
-export default AdBanner;
+        <Head />
         <body>
           <Main />
           <NextScript />
